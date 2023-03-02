1b290ef34bfbf62ce6a175a47ac8601755f9063c
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2023-03-02 17:37:13 -0600

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -20,6 +20,20 @@ int Date::get_month() { return this->month; }
 
 int Date::get_year() { return this->year; }
 
+bool Date:: operator <= (const Date& other) {
+    if (this->year < other.year) { return true; }
+    else if (this->year > other.year) { return false; }
+    else if (this->year == other.year) {
+        if (this->month < other.month) { return true; }
+        else if (this->month > other.month) { return false; }
+        else if (this->month == other.month) {
+            if (this->day < other.day) { return true; }
+            else if (this->day > other.day) { return false; }
+            else { return true; }
+        }
+    }
+}
+
 std::ostream& operator << (std::ostream& os, const Date& date) {
     os << std::setw(2) << std::setfill('0') << date.day << "/";
     os << std::setw(2) << std::setfill('0') << date.month << "/";
