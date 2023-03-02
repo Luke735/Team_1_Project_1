54f1f54f65031e28d6374451ea83de2284ac2a7a
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2023-03-02 16:28:19 -0600

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -23,6 +23,8 @@ public:
 	bool get_status();
 	Date get_release_date();
 	Date get_recieve_date();
+	
+	friend std::ostream& operator<<(std::ostream& os, const Movie& movie);
 private:
 	Date release_date, recieve_date;
 	string name, description;
